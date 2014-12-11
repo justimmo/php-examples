d1fae60856fa9d7e8f7de76132ceef569f0d655b
Simon Mitterhofer <Tschebel@users.noreply.github.com>
2014-12-11 10:52:06 +0100

    add user/pw for test data

--- config.php
+++ config.php
@@ -6,8 +6,8 @@ use Justimmo\Api\JustimmoApi;
 use Psr\Log\NullLogger;
 use Justimmo\Cache\NullCache;
 
-$api_username = 'username';
-$api_password = 'password';
+$api_username = 'api-7586';
+$api_password = 'FkAIJ2depD';
 
 $api = new JustimmoApi($api_username, $api_password, new NullLogger(), new NullCache());
 
